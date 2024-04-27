5cef038c5299e93992a4e7f56dc88b6c5d150567
kimmi <kimminjea31@naver.com>
2024-04-28 00:53:10 +0900

    동적할당 및 설명 수정

--- opensource_midproject/opensource_mid_project.c
+++ opensource_midproject/opensource_mid_project.c
@@ -5,23 +5,26 @@
 #include <stdlib.h>
 
 #define MAX_TITLE_LENGTH 100
-#define MAX_CONTENT_LENGTH 1000
+#define MAX_CONTENT_LENGTH 1024
 #define MAX_PATH_LENGTH 200
 
 
 //저자 폴더 만드는 함수
 void make_dir_folder(const char* name)
 {
-    char folderPath[MAX_PATH];
-    strcpy(folderPath, "C:\\Users\\kimmi\\Desktop\\독후감상문\\"); // 바탕화면에 입력한 이름으로 폴더 생성
-    strcat(folderPath, name); // 원래있던 기본 주소와 입력한 만들 폴더 이름과 합침.
+    char* folderPath;
+    folderPath = (char*)malloc(MAX_CONTENT_LENGTH * sizeof(char));
+    strcpy(folderPath, "C:\\Users\\kimmi\\Desktop\\독후감상문\\"); // 독후감상문 폴더에 저자명으로 폴더 생성
+    strcat(folderPath, name); // 원래있던 기본 주소와 저자명과 합침.
 
     BOOL result = CreateDirectoryA(folderPath, NULL);
 
-    if (result) {
+    if (result) 
+    {
         printf("감상문 작성 완료.\n");
     }
-    else {
+    else 
+    {
         DWORD error = GetLastError();
         if (error == ERROR_ALREADY_EXISTS) {
             printf("저자명 폴더가 존재합니다. \n");
@@ -30,26 +33,32 @@ void make_dir_folder(const char* name)
             printf("폴더를 만드는 데 실패했습니다. 오류 코드: %lu\n", error);
         }
     }
+    free(folderPath);
 }
 
 //감상문 파일을 만드는 함수
 void make_write_text(const char* folder_name, const char* input_title, const char* input_content)
 {
-    char folderPath[MAX_PATH];
+    char* folderPath;
+    folderPath = (char*)malloc(MAX_CONTENT_LENGTH * sizeof(char));
     sprintf(folderPath, "C:\\Users\\kimmi\\Desktop\\독후감상문\\%s\\", folder_name); // 폴더 경로 생성
 
-    char filePath[MAX_PATH];
+    char* filePath;
+    filePath = (char*)malloc(MAX_CONTENT_LENGTH * sizeof(char));
     sprintf(filePath, "%s%s.txt", folderPath, input_title); // 파일 경로 생성
 
     // 폴더가 이미 존재하는지 확인
     DWORD attrib = GetFileAttributesA(folderPath);
-    if (attrib != INVALID_FILE_ATTRIBUTES && (attrib & FILE_ATTRIBUTE_DIRECTORY)) {
+    if (attrib != INVALID_FILE_ATTRIBUTES && (attrib & FILE_ATTRIBUTE_DIRECTORY)) 
+    {
         printf("폴더가 이미 존재합니다.\n");
     }
-    else {
+    else 
+    {
         // 폴더 생성
         BOOL result = CreateDirectoryA(folderPath, NULL);
-        if (!result) {
+        if (!result) 
+        {
             DWORD error = GetLastError();
             printf("폴더를 만드는 데 실패했습니다. 오류 코드: %lu\n", error);
             return;
@@ -58,49 +67,61 @@ void make_write_text(const char* folder_name, const char* input_title, const cha
 
     FILE* filePointer = fopen(filePath, "w"); // "w" 쓰기 모드로 파일 열기
 
-    if (filePointer == NULL) {
+    if (filePointer == NULL) 
+    {
         printf("파일을 열 수 없습니다.\n");
         return;
     }
 
-    // 내용 입력
+    // txt 파일에 내용 입력
     fprintf(filePointer, "%s", input_content);
 
     fclose(filePointer);
 
     printf("파일이 성공적으로 생성되었습니다.\n");
+    free(folderPath);
+    free(filePath);
 }
 
-//감상문 폴더를 출력 함수
+//감상문 파일를 출력 함수
 void read_text(const char* folder_name, const char* input_title)
 {
-    char filePath[MAX_PATH_LENGTH];
+    char* filePath;
+    filePath = (char*)malloc(MAX_CONTENT_LENGTH * sizeof(char));
     sprintf(filePath, "C:\\Users\\kimmi\\Desktop\\독후감상문\\%s\\%s.txt", folder_name, input_title); // 파일 경로 생성
 
     FILE* filePointer = fopen(filePath, "r"); // "r" 모드로 파일 열기
 
-    if (filePointer == NULL) {
+    if (filePointer == NULL) 
+    {
         printf("파일을 열 수 없습니다.\n");
         return;
     }
 
     // 파일에서 한 줄씩 읽어와 출력하기
-    char buffer[100]; // 읽은 내용을 저장할 버퍼
-    while (fgets(buffer, sizeof(buffer), filePointer) != NULL) {
+    char*buffer;
+    buffer = (char*)malloc(MAX_CONTENT_LENGTH * sizeof(char));// 읽은 내용을 저장할 버퍼
+    while (fgets(buffer, sizeof(buffer), filePointer) != NULL) 
+    {
         printf("%s", buffer);
     }
 
     fclose(filePointer); // 파일 닫기
+    free(filePath);
+    free(buffer);
 }
 
 //감상문 내용을 수정 함수
-void edit_text(const char* folder_name, const char* input_title, const char* input_content) {
-    char filePath[MAX_PATH_LENGTH];
+void edit_text(const char* folder_name, const char* input_title, const char* input_content) 
+{
+    char* filePath;
+    filePath = (char*)malloc(MAX_CONTENT_LENGTH * sizeof(char));
     sprintf(filePath, "C:\\Users\\kimmi\\Desktop\\독후감상문\\%s\\%s.txt", folder_name, input_title); // 파일 경로 생성
 
     FILE* filePointer = fopen(filePath, "w"); // "w" 쓰기 모드로 파일 열기
 
-    if (filePointer == NULL) {
+    if (filePointer == NULL) 
+    {
         printf("파일을 열 수 없습니다.\n");
         return;
     }
@@ -111,26 +132,41 @@ void edit_text(const char* folder_name, const char* input_title, const char* inp
     fclose(filePointer);
 
     printf("파일이 성공적으로 덮어쓰기 되었습니다.\n");
+    free(filePath);
 }
 
 //감상문 text파일을 삭제 함수
-void delete_text(const char* folder_name, const char* input_title) {
-    char filePath[MAX_PATH_LENGTH];
+void delete_text(const char* folder_name, const char* input_title) 
+{
+    char* filePath;
+    filePath = (char*)malloc(MAX_CONTENT_LENGTH * sizeof(char));
     sprintf(filePath, "C:\\Users\\kimmi\\Desktop\\독후감상문\\%s\\%s.txt", folder_name, input_title); // 파일 경로 생성
+    
+    char check[4];
+    printf("삭제하시겠습니까? (복구할 수 없습니다) \n");
+    printf("yes/no ==>>");
+    scanf("%3s", check);
 
-    // 파일 삭제
-    if (remove(filePath) == 0) {
-        printf("파일이 성공적으로 삭제되었습니다.\n");
+    if (strcmp(check, "yes") == 0)
+    {
+        // 파일 삭제
+        if (remove(filePath) == 0) 
+        {
+            printf("\n 파일이 성공적으로 삭제되었습니다.\n");
+        }
+        else {
+            printf("파일 삭제에 실패했습니다.\n");
+        }
     }
-    else {
-        printf("파일 삭제에 실패했습니다.\n");
-    }
+    free(filePath);
 }
 
 //책 추천해주는 함수
-void recommend_book(const char* filename) {
+void recommend_book(const char* filename) 
+{
     FILE* file = fopen(filename, "r");
-    if (file == NULL) {
+    if (file == NULL) 
+    {
         perror("파일 열기 실패");
         return;
     }
@@ -138,8 +174,10 @@ void recommend_book(const char* filename) {
     // 파일에서 총 줄 수를 계산
     int lines = 0;
     char ch;
-    while ((ch = fgetc(file)) != EOF) {
-        if (ch == '\n') {
+    while ((ch = fgetc(file)) != EOF) 
+    {
+        if (ch == '\n') 
+        {
             lines++;
         }
     }
@@ -153,16 +191,17 @@ void recommend_book(const char* filename) {
 
     // 선택된 줄까지 이동
     int current_line = 0;
-    char buffer[1024];
-    char* title = NULL;
-    title = (char*)malloc(1024 * sizeof(char)); // 제목 공간 동적할당
-    char* author = NULL;
-    author = (char*)malloc(1024 * sizeof(char));// 저자 공간 동적할당
-    while (fgets(buffer, sizeof(buffer), file) != NULL) {
-        if (current_line == random_line) {
+    char buffer[MAX_CONTENT_LENGTH]; // 문자 배열
+    char title[MAX_CONTENT_LENGTH]; // 제목 문자 배열
+    char author[MAX_CONTENT_LENGTH]; // 저자 문자 배열
+    while (fgets(buffer, sizeof(buffer), file) != NULL) 
+    {
+        if (current_line == random_line) 
+        {
             // 줄에서 "//"를 기준으로 제목과 저자 분리
             char* slash_pos = strstr(buffer, "//");
-            if (slash_pos != NULL) {
+            if (slash_pos != NULL) 
+            {
                 *slash_pos = '\0'; // "//" 이전 내용은 제목
                 strcpy(title, buffer);
                 strcpy(author, slash_pos + 2); // "//" 이후 내용은 저자
@@ -176,16 +215,15 @@ void recommend_book(const char* filename) {
     printf("제목 : %s\n", title);
     printf("저자 : %s\n", author);
 
-    free(title);
-    free(author);
-
     fclose(file);
 }
 
 //책 추천 목록 추가 함수
-void append_to_file(const char* filename, const char* new_data) {
+void add_recommend_book(const char* filename, const char* new_data) 
+{
     FILE* file = fopen(filename, "a");
-    if (file == NULL) {
+    if (file == NULL) 
+    {
         perror("파일 열기 실패");
         return;
     }
@@ -229,7 +267,8 @@ int main()
             // 폴더명 입력 받기
             printf("저자명을 입력하세요 : ");
             input_folder = (char*)malloc(MAX_TITLE_LENGTH * sizeof(char));
-            if (input_folder == NULL) {
+            if (input_folder == NULL) 
+            {
                 printf("메모리 할당 실패\n");
                 return 1;
             }
@@ -239,7 +278,8 @@ int main()
             // 제목 입력 받기
             printf("제목을 입력하세요 : ");
             input_title = (char*)malloc(MAX_TITLE_LENGTH * sizeof(char));
-            if (input_title == NULL) {
+            if (input_title == NULL) 
+            {
                 printf("메모리 할당 실패\n");
                 free(input_folder); // 폴더명 메모리 해제
                 return 1;
@@ -250,17 +290,20 @@ int main()
             // 내용 입력 받기
             printf("내용을 입력하세요. 입력을 종료하려면 '// '을 입력하세요.\n");
             input_content = (char*)malloc(MAX_CONTENT_LENGTH * sizeof(char));
-            if (input_content == NULL) {
+            if (input_content == NULL) 
+            {
                 printf("메모리 할당 실패\n");
                 free(input_folder); // 폴더명 메모리 해제
                 free(input_title);  // 제목 메모리 해제
                 return 1;
             }
             input_content[0] = '\0'; // 내용 배열 초기화
-            while (1) {
+            while (1) 
+            {
                 fgets(input_content + strlen(input_content), MAX_CONTENT_LENGTH - strlen(input_content), stdin); // 사용자 입력 받기
                 // "//" 입력하면 종료
-                if (strncmp(input_content + strlen(input_content) - 3, "//", 2) == 0) {
+                if (strncmp(input_content + strlen(input_content) - 3, "//", 2) == 0) 
+                {
                     break;
                 }
             }
@@ -281,7 +324,8 @@ int main()
             // 저자명 입력 받기
             printf("저자명을 입력하세요 : ");
             input_folder_r = (char*)malloc(MAX_PATH_LENGTH * sizeof(char));
-            if (input_folder_r == NULL) {
+            if (input_folder_r == NULL) 
+            {
                 printf("메모리 할당 실패\n");
                 return 1;
             }
@@ -291,7 +335,8 @@ int main()
             // 제목 입력 받기
             printf("제목을 입력하세요 : ");
             input_title_r = (char*)malloc(MAX_PATH_LENGTH * sizeof(char));
-            if (input_title_r == NULL) {
+            if (input_title_r == NULL) 
+            {
                 printf("메모리 할당 실패\n");
                 free(input_folder_r); // 동적 할당 해제
                 return 1;
@@ -316,7 +361,8 @@ int main()
             // 저자명 입력 받기
             printf("저자명을 입력하세요: ");
             input_folder_e = (char*)malloc(MAX_TITLE_LENGTH * sizeof(char));
-            if (input_folder_e == NULL) {
+            if (input_folder_e == NULL) 
+            {
                 printf("메모리 할당 실패\n");
                 return 1;
             }
@@ -326,7 +372,8 @@ int main()
             // 제목 입력 받기
             printf("제목을 입력하세요: ");
             input_title_e = (char*)malloc(MAX_TITLE_LENGTH * sizeof(char));
-            if (input_title_e == NULL) {
+            if (input_title_e == NULL) 
+            {
                 printf("메모리 할당 실패\n");
                 free(input_folder_e); // 동적 할당 해제
                 return 1;
@@ -337,7 +384,8 @@ int main()
             // 내용 입력 받기
             printf("내용을 입력하세요: ");
             input_content_e = (char*)malloc(MAX_CONTENT_LENGTH * sizeof(char));
-            if (input_content_e == NULL) {
+            if (input_content_e == NULL) 
+            {
                 printf("메모리 할당 실패\n");
                 free(input_folder_e); // 동적 할당 해제
                 free(input_title_e); // 동적 할당 해제
@@ -363,7 +411,8 @@ int main()
             // 저자명 입력 받기
             printf("저자명을 입력하세요: ");
             input_folder_d = (char*)malloc(MAX_PATH_LENGTH * sizeof(char));
-            if (input_folder_d == NULL) {
+            if (input_folder_d == NULL) 
+            {
                 printf("메모리 할당 실패\n");
                 return 1;
             }
@@ -373,7 +422,8 @@ int main()
             // 제목 입력 받기
             printf("제목을 입력하세요: ");
             input_title_d = (char*)malloc(MAX_PATH_LENGTH * sizeof(char));
-            if (input_title_d == NULL) {
+            if (input_title_d == NULL) 
+            {
                 printf("메모리 할당 실패\n");
                 free(input_folder_d); // 동적 할당 해제
                 return 1;
@@ -404,7 +454,7 @@ int main()
             scanf(" %[^\n]", add_data); // 개행 문자를 제외한 문자열 입력 받기
 
             // 파일에 내용 추가
-            append_to_file(add_filename, add_data);
+            add_recommend_book(add_filename, add_data);
 
             printf("성공적으로 추가되었습니다.\n");
             break;
